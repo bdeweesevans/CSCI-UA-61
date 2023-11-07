fe4a866850067d84da9344ea5ccc0595d47e9c5c
bdeweesevans <bdeweesevans@gmail.com>
2023-11-06 21:33:52 -0500

    assignment 5

---
+++ asg5/newNodes.js
@@ -0,0 +1,22 @@
+function createNavItem(text, href) {
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const a = document.createElement('a');
+    a.href = href;
+    a.textContent = text;
+    h3.appendChild(a);
+    li.appendChild(h3);
+    return li;
+}
+
+document.getElementById('nodeButton').addEventListener('click', function () {
+    const myList = document.getElementById('myList');
+
+    myList.appendChild(createNavItem('Assignments', '/~bdd6280/header.html'));
+    myList.appendChild(createNavItem('Claim a prize on this page', '/~bdd6280/asg3.html'));
+
+    const h3Tags = myList.querySelectorAll('a');
+    h3Tags.forEach(function(anchor) {
+        anchor.style.color = 'green';
+    });
+});
