786227557ce8b2bf3fa55c0863a3ff693d603d91
bdeweesevans <bdeweesevans@gmail.com>
2023-11-26 15:04:04 -0500

    calculator application complete

--- asg7/add.php
+++ asg7/add.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $number1 = $_POST['number1'];
-    $number2 = $_POST['number2'];
+    $number1 = (float)$_POST['number1'];
+    $number2 = (float)$_POST['number2'];
+
     $operator = $_POST['operator'];
 
     $result = 0;
